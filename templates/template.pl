493ca22fc0fadeb1ff7934fa23e27818c1e3833c
Brandon Casey(Work) <iN7h33nD@gmail.com>
2014-03-21 12:54:59 -0400

    adding a template for perl scripts

---
+++ templates/template.pl
@@ -0,0 +1,16 @@
+#! /usr/bin/env perl
+#
+# Short description for %FFILE%
+#
+# Author %USER% <%MAIL%>
+# Version 0.1
+# Copyright (C) %YEAR% %USER% <%MAIL%>
+# Modified On %FDATE%
+# Created  %FDATE%
+#
+use strict;
+use warnings;
+
+
+
+%HERE%
