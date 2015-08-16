e88be5cc8d3c97027cd41cc771d8c869166080b0
Adrian Perez de Castro <aperez@igalia.com>
2015-08-17 00:41:29 +0300

    Add Rust language template

---
+++ templates/=template=.rs
@@ -0,0 +1,17 @@
+//
+// %FFILE%
+// Copyright (C) %YEAR% %USER% <%MAIL%>
+// Distributed under terms of the %LICENSE% license.
+//
+
+%HERE%
+
+
+#[cfg(tests)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_works() {
+	}
+}
