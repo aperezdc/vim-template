99f4756e9975acea30f331bb901132db83986bdd
ccsplit <cshay237@gmail.com>
2019-09-12 10:38:52 -0500

    Fixed the python template.

--- templates/=template=.py
+++ templates/=template=.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
@@ -16,8 +16,9 @@ Options:
 """
 from docopt import docopt
 
+
 def main(args):
-    print args
+    print(args)
     %HERE%
 
 if __name__ == '__main__':
